d7e1132780da23fcff4ef4871ad6ca709784ee61
kzorin52 <kzorin52@gmail.com>
2020-12-19 22:55:14 +0600

    Fix Marks

--- DnevnikRuAPI.cs
+++ DnevnikRuAPI.cs
@@ -17,9 +17,9 @@ namespace DnevnikRuAPI
             using (var request = new HttpRequest())
             {
                 var stringContent = new StringContent(
-                    $"{{  \"agreeTerms\": true,  \"clientId\": \"1d7bd105-4cd1-4f6c-9ecc-394e400b53bd\", " +
+                    "{  \"agreeTerms\": true,  \"clientId\": \"1d7bd105-4cd1-4f6c-9ecc-394e400b53bd\", " +
                     $" \"clientSecret\": \"5dcb5237-b5d3-406b-8fee-4441c3a66c99\",  \"password\": \"{password}\",  " +
-                    $"\"scope\": \"Schools,Relatives,EduGroups,Lessons,marks,EduWorks,EducationalInfo,Avatar\", " +
+                    "\"scope\": \"Schools,Relatives,EduGroups,Lessons,marks,EduWorks,EducationalInfo,Avatar\", " +
                     $" \"username\": \"{login}\"}}",
                     Encoding.UTF8);
 
@@ -111,7 +111,7 @@ namespace DnevnikRuAPI
 
         public static class Marks
         {
-            public static Mark GetMarkByLesson(long lessonId)
+            public static List<Mark> GetMarkByLesson(long lessonId)
             {
                 var content = "";
                 using (var request = new HttpRequest())
@@ -125,27 +125,27 @@ namespace DnevnikRuAPI
                     content = response.ToString();
                 }
 
-                return JsonConvert.DeserializeObject<Mark>(content);
+                return JsonConvert.DeserializeObject<List<Mark>>(content);
             }
 
             public class Mark
             {
-                public long id { get; set; } 
-                public string id_str { get; set; } 
-                public string type { get; set; } 
-                public string value { get; set; } 
-                public string textValue { get; set; } 
-                public long person { get; set; } 
-                public string person_str { get; set; } 
-                public long work { get; set; } 
-                public string work_str { get; set; } 
-                public long lesson { get; set; } 
-                public string lesson_str { get; set; } 
-                public int number { get; set; } 
-                public DateTime date { get; set; } 
-                public long workType { get; set; } 
-                public string mood { get; set; } 
-                public bool use_avg_calc { get; set; } 
+                public long id { get; set; }
+                public string id_str { get; set; }
+                public string type { get; set; }
+                public string value { get; set; }
+                public string textValue { get; set; }
+                public long person { get; set; }
+                public string person_str { get; set; }
+                public long work { get; set; }
+                public string work_str { get; set; }
+                public long lesson { get; set; }
+                public string lesson_str { get; set; }
+                public int number { get; set; }
+                public DateTime date { get; set; }
+                public long workType { get; set; }
+                public string mood { get; set; }
+                public bool use_avg_calc { get; set; }
             }
         }
 
@@ -176,27 +176,27 @@ namespace DnevnikRuAPI
 
             public class Homework
             {
-                public long id { get; set; } 
-                public string id_str { get; set; } 
-                public string type { get; set; } 
-                public long workType { get; set; } 
-                public string markType { get; set; } 
-                public int markCount { get; set; } 
-                public long lesson { get; set; } 
-                public string lesson_str { get; set; } 
-                public bool displayInJournal { get; set; } 
-                public string status { get; set; } 
-                public long eduGroup { get; set; } 
-                public string eduGroup_str { get; set; } 
-                public string text { get; set; } 
-                public int periodNumber { get; set; } 
-                public string periodType { get; set; } 
-                public long subjectId { get; set; } 
-                public bool isImportant { get; set; } 
-                public DateTime targetDate { get; set; } 
-                public object sentDate { get; set; } 
-                public long createdBy { get; set; } 
-                public List<object> files { get; set; } 
+                public long id { get; set; }
+                public string id_str { get; set; }
+                public string type { get; set; }
+                public long workType { get; set; }
+                public string markType { get; set; }
+                public int markCount { get; set; }
+                public long lesson { get; set; }
+                public string lesson_str { get; set; }
+                public bool displayInJournal { get; set; }
+                public string status { get; set; }
+                public long eduGroup { get; set; }
+                public string eduGroup_str { get; set; }
+                public string text { get; set; }
+                public int periodNumber { get; set; }
+                public string periodType { get; set; }
+                public long subjectId { get; set; }
+                public bool isImportant { get; set; }
+                public DateTime targetDate { get; set; }
+                public object sentDate { get; set; }
+                public long createdBy { get; set; }
+                public List<object> files { get; set; }
                 public List<object> oneDriveLinks { get; set; }
                 public string lessonName => Lessons.GetLesson(lesson).subject.name;
             }
